bad53999bd73a814ef9e41ea4aa826f076824565
justinyiwang <justinyiwang@gmail.com>
2017-06-05 10:24:25 -0700

    Chapter 5 Homework

---
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,4 @@
+puts "Enter your favorite number"
+number = gets.chomp.to_i
+number = number+1
+puts number.to_s + " is a bigger and better number"
---
+++ ch05/fullNameGreeting.rb
@@ -0,0 +1,7 @@
+puts "Please enter your first name:"
+first = gets.chomp
+puts "Please enter your middle name:"
+middle = gets.chomp
+puts "Please enter your last name:"
+last = gets.chomp
+puts "Hello, " + first + " " + middle + " " + last
