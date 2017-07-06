c70e70eb2f0dc8889c716934675352bd585b300c
justinyiwang <justinyiwang@gmail.com>
2017-07-06 05:34:09 -0700

    used array in different way

--- ch08/tableOfContents.rb
+++ ch08/tableOfContents.rb
@@ -1,3 +1,7 @@
-line_width = 40
-tbc = ["Table of Contents".center(line_width),"Chapter 1: Getting Started".ljust(line_width/2)+"page  1".rjust(line_width/2),"Chapter 2: Numbers        ".ljust(line_width/2)+"page  9".rjust(line_width/2),"Chapter 3: Letters        ".ljust(line_width/2)+"page 13".rjust(line_width/2)]
-puts tbc
+tbc = [['Table of Contents'],['Chapter 1: Getting Started', 'page  1'],['Chapter 2: Numbers        ', 'page  9'],['Chapter 3: Letters        ', 'page 13']]
+width = 40
+hwidth = width/2
+puts tbc[0][0].center(width)
+puts tbc[1][0].ljust(hwidth)+tbc[1][1].rjust(hwidth)
+puts tbc[2][0].ljust(hwidth)+tbc[2][1].rjust(hwidth)
+puts tbc[3][0].ljust(hwidth)+tbc[3][1].rjust(hwidth)
